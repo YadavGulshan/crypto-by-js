548d59c8985d3d3a9294263102ca59bf0e81fc35
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-12 12:00:50 +0530

    Added solana spl and web3 library

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [provider, setProvider] = useState();
+  const [loading, setLoading] = useState();
+
+
+  const getProvider = () =>{
+    if("solana" in window){
+      const provider = window.solana;
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>
+        Create your own token using JS
+      </h1>
+
+      {
+        walletConnected ? (
+          <p>
+            <strong>
+              Public key:
+            </strong>
+            {provider.publicKey.toString()}
+          </p>
+        ) : (
+          <p>
+          </p>
+        )
+      }
+    </>
   );
 }
 
