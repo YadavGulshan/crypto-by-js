13bbb0bda33f1343dd74b78f34f8cdc076ccb7f0
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-14 17:36:06 +0530

    Typing effect done

--- src/components/typing.jsx
+++ src/components/typing.jsx
@@ -2,41 +2,47 @@ import React from "react";
 import Typed from "typed.js";
 
 const words = [
-    'Javascript Consultant',
-    'Cloud Architect',
-    'Open source contributor'
-  ]
-  
-class Typing extends React.Component{
-    componentDidMount() {
-        const options = {
-          strings: words,
-          typeSpeed: 50,
-          backSpeed: 50,
-          loop: true,
-          cursorChar: "|",
-          
-        };
-        // this.el refers to the <span> in the render() method
-        this.typed = new Typed(this.el, options);
-      }
-    componentWillUnmount() {
-        // Please don't forget to cleanup animation layer
-        this.typed.destroy();
-      }
-      
-      render() {
-        return (
-          <>
-            <span
-              style={{ fontSize: "40px" }}
-              ref={(el) => {
-                this.el = el;
-              }}
-            />
-          </>
-        );
-      }
+  'my Dapp',
+  'your Dapp',
+  'our own open source Dapp!',
+]
+
+const options = {
+  strings: words,
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+  cursorChar: "|",
+
+};
+
+class Typing extends React.Component {
+  componentDidMount() {
+    // this.el refers to the <span> in the render() method
+    this.typed = new Typed(this.el, options);
+  }
+  componentWillUnmount() {
+    // Please don't forget to cleanup animation layer
+    this.typed.destroy();
+  }
+
+  render() {
+    return (
+      <>
+      <div>
+        Welcome to&nbsp;
+        <span
+          style={{  }}
+          ref={(el) => {
+            this.el = el;
+          }}
+          // className='typed-cursor'
+        />
+      </div>
+        
+      </>
+    );
+  }
 }
 
 export default Typing;
