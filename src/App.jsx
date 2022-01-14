c23e064726b09fedefbaaf093ee7d595bd092347
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-14 23:02:26 +0530

    Added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import './App.css';
-import Card from './components/card';
+import Card2 from './components/card2';
 import { useMyContext } from './context/context'
 const App = () => {
-  const { loading, walletConnected, walletConnectionHelper, provider} = useMyContext();
+  const { loading, walletConnected, walletConnectionHelper, provider } = useMyContext();
+  const { airDropHelper, supplyCapped, reMintHelper, initialMintHelper, transferTokenHelper, mintingWalletSecretKey, isSupplyCapped, capTheSupply } = useMyContext()
   return (
     <div className="h-screen relative overflow-x-clip">
-      <Card />
-      <div className="mr-44 ml-44 flex justify-between items-center absolute md:m-44 gap-10 bg-black opacity-90 p-20 rounded-xl">
-        <div className="text-center w-60 md:text-left md:w-1/3 flex flex-col gap-10">
-          <p className="text-lg">
-            Culpa ipsum exercitation esse labore Lorem mollit. Quis magna consectetur duis id consectetur esse eiusmod. Aliqua cillum commodo consequat ea aliqua consequat veniam labore ad amet.
-            <br/>
-            Culpa ipsum exercitation esse labore Lorem mollit. Quis magna consectetur duis id consectetur esse eiusmod. Aliqua cillum commodo consequat ea aliqua consequat veniam labore ad amet.
-            <br/>
-            Culpa ipsum exercitation esse labore Lorem mollit. Quis magna consectetur duis id consectetur esse eiusmod. Aliqua cillum commodo consequat ea aliqua consequat veniam labore ad amet.
-          </p>
-          <button className="title w-40 h-10 rounded-lg">
+      {/* <Card /> */}
+      <Card2 />
+      <div className="mr-52 ml-52 flex justify-between items-center absolute md:m-44 gap-10 bg-black opacity-90 p-20 rounded-xl">
+        <div className=" w-60 md:text-left md:w-1/3 flex flex-col gap-5 ">
+          <button onClick={airDropHelper} disabled={loading} className="bg-white text-black w-40 h-10 rounded-lg">AirDrop</button>
+          <button disabled={loading || supplyCapped} onClick={reMintHelper} className="bg-white text-black w-40 h-10 rounded-lg">Mint Again</button>
+          <button disabled={loading} onClick={initialMintHelper} className="bg-white text-black w-40 h-10 rounded-lg">Create Your Own Token</button>
+          <button disabled={loading} onClick={transferTokenHelper} className="bg-white text-black w-40 h-10 rounded-lg">Transfer Token</button>
+          {isSupplyCapped ? (<div>
+            Fuck, Supply is capped
+          </div>) : (<div>
+            <button onClick={capTheSupply} disabled={isSupplyCapped} className="bg-white text-black w-40 h-10 rounded-lg">Cap it!</button>
+          </div>)}
+          <button className="bg-white text-black w-40 h-10 rounded-lg">
             Show More!
           </button>
         </div>
@@ -23,27 +27,27 @@ const App = () => {
         {/* Card here! */}
         <div className="wallet-card text-white font-medium rounded-lg ">
           {
-          loading ?
-          <div className="h-96 w-96 text-center">Loading...</div> :
-          (
-            <div className="flex flex-row justify-between p-10 h-96">
-              {
-                walletConnected ? (
-                  <div className="flex flex-col justify-end text-black">
-                      <strong>
-                      {provider.publicKey.toString()}
-                      </strong>
+            loading ?
+              <div className="h-96 w-96 text-center">Loading...</div> :
+              (
+                <div className="flex flex-row justify-between p-10 h-96">
+                  {
+                    walletConnected ? (
+                      <div className="flex flex-col justify-end text-black">
+                        <strong>
+                          {provider.publicKey.toString()}
+                        </strong>
 
-                  </div>
-                ) : (
-                  <div className="w-96"></div>
-                )
-              }
-              <button onClick={walletConnectionHelper} disabled={loading} className="h-5">
-                {!walletConnected ? "Connect Wallet" : "Disconnect Wallet"}
-              </button>
-            </div>
-          )}
+                      </div>
+                    ) : (
+                      <div className="w-96"></div>
+                    )
+                  }
+                  <button onClick={walletConnectionHelper} disabled={loading} className="h-5">
+                    {!walletConnected ? "Connect Wallet" : "Disconnect Wallet"}
+                  </button>
+                </div>
+              )}
         </div>
       </div>
     </div>
---
+++ src/components/dialog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const dialog = () => {
+    return (
+        <div className="absolute bg-white text-black h-1/2 w-1/2">
+            hey
+        </div>
+    )
+}
+
+export default dialog
